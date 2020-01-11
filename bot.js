d0a4655a9ce894c35e17fa265f175401ae882911
nikhilreddy09 <gudibandisainikhilreddy@gmail.com>
2020-01-10 20:04:28 -0500

    added html file for user input

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 //npm module to count number of hashtags
 var HashtagCount = require("hashtag-count")
+const express = require('express')
+var bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 var hc = new HashtagCount({
@@ -9,8 +12,29 @@ var hc = new HashtagCount({
   'access_token_secret': process.env.ACCESS_TOKEN_SECRET
 });
 
-// //giving the hashtags for which we want to see the count
- var hashtags = ['obama', 'trump', 'modi'];
+const app = express()
+const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.get('/', function (req, res, next) {
+  res.sendfile('./home.html');
+});
+
+app.post('/submit-form' , (req,res) => {
+  const name1 = req.body.name1
+  const name2 = req.body.name2
+  const name3 = req.body.name3
+  console.log(name1 , name2 , name3)
+
+  count(name1,name2,name3)
+
+  res.end()
+})
+
+var count = function(name1,name2,name3) {
+//giving the hashtags for which we want to see the count
+ var hashtags = [name1, name2,name3];
 // //time interval
  var interval = '15 seconds';
 //time limit for the program
@@ -27,7 +51,7 @@ var finishedCb = function (err, results) {
       //get the object from the final array
       let final = answer[0]
       //get individual elements from the object
-      console.log(final.obama)
+      console.log(final)
       //get the total object values.
       console.log(Object.values(obj));
   }
@@ -39,5 +63,12 @@ hc.start({
   limit: limit,             
   finishedCb: finishedCb,   
 });
+
+}
+app.listen(port , () => {
+  console.log("server is up on " + port)
+ })
+ 
+
 
 
